1e97133fda553c9269756fabaed05cf847ab6561
Yura <savenkov@cmd.su>
2021-11-17 22:08:48 +0300

    03-objects-arrays-intro-to-testing finishing homework

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (typeof size === 'undefined') {
+    return string;
+  }
+  let result = '';
+  let counter = 1;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i + 1]) {
+      counter++;
+    } else {
+      counter = 1;
+    }
 
+    if (counter <= size) {
+      result += string[i];
+    }
+  }
+  return result;
 }
